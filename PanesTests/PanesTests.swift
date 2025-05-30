1359dc6e7c2802beb513287ad041e93c4ea8f5fc
Sam Deane <sam@elegantchaos.com>
2025-05-30 09:26:20 +0100

    first commit

---
+++ PanesTests/PanesTests.swift
@@ -0,0 +1,17 @@
+//
+//  PanesTests.swift
+//  PanesTests
+//
+//  Created by Sam Deane on 30/05/2025.
+//
+
+import Testing
+@testable import Panes
+
+struct PanesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
